d60d41dae6a68d8f8e98ae6f19a3ab32d340fb87
Leonardo Bezerra <leobezerra@imd.ufrn.br>
2017-06-16 02:04:59 -0300

    [Projeto] Port forwarding: leitor de buffer completo.

--- projetos/port-forwarding/files/read-datagrams.cpp
+++ projetos/port-forwarding/files/read-datagrams.cpp
@@ -149,7 +149,6 @@ int main (int argc, char * argv[]) {
 
     std::string line;
     while (std::getline(std::cin, line)) {
-        std::cout << "line: " << line << std::endl;
         std::istringstream in(line);
         in.setf(std::ios::hex, std::ios::basefield);
 
@@ -160,7 +159,7 @@ int main (int argc, char * argv[]) {
         ushort dscp = dscp_ >> 2;
         ushort total = (total_1 << 8) + total_2;
 
-        std::cout << "ihl: " << ihl << ", dscp: " << dscp << ", total: " << total << std::endl;
+        std::cout << "ihl: " << ihl << ", dscp: " << dscp << ", total_length: " << total << std::endl;
 
         std::getline(std::cin, line);
         std::getline(std::cin, line);
@@ -170,18 +169,35 @@ int main (int argc, char * argv[]) {
         in >> ip1 >> ip2 >> ip3 >> ip4;
         std::string ip = std::to_string(ip1) + "." + std::to_string(ip2) + "." + std::to_string(ip3) + "." + std::to_string(ip4);
 
-        for (unsigned i = 5; i < ihl; i++) std::getline(std::cin, line);
-        in.str(line);
+        if (ihl > 5) {
+            for (unsigned i = 5; i < ihl; i += 2) std::getline(std::cin, line);
+            in.str(line);
+            in.clear();
+            if (ihl % 2) in.ignore(12);
+        }
         in.ignore(6);
         ushort porta_1, porta_2;
         in >> porta_1 >> porta_2;
         ushort porta = (porta_1 << 8) + porta_2;
 
-        std::cout << "ip: " << ip << ", porta: " << porta << std::endl;
+        std::cout << "ip: " << ip << ", port: " << porta << std::endl;
         
         short total_data = total - ihl * 4;
         std::string data_hex;
-        while (total_data > 0) { 
+        std::ostringstream out;
+        ushort tmp;
+        if (ihl % 2) {
+            std::getline(std::cin, line);
+            in.str(line);
+            in.clear();
+            in.ignore(12);
+            for (ushort i = 0; i < 4; i++) {
+                in >> line;
+                data_hex += line;
+                total_data--;
+            }
+        }
+        while (total_data > 0) {
             std::cin >> line;
             data_hex += line;
             total_data--;
